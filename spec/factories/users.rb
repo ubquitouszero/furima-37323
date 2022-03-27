ea9dac5ce22b140362f56446255d020ffe4ebf32
ubquitouszero <96573721+ubquitouszero@users.noreply.github.com>
2022-03-27 19:20:47 +0900

    FactoryBot設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :user do
+    transient do
+      person { Gimei.name }
+    end
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6, mix_case: true)}
     password_confirmation {password}
-    last_name              {Faker::Person.last.kanji}
-    first_name             {Faker::Person.first.kanji}
-    last_name_kana              {Faker::Name.Person.last.katakana}
-    first_name_kana              {Faker::Name.Person.first.katakana}
+    last_name              {person.last.kanji}
+    first_name             {person.first.kanji}
+    last_name_kana              {person.last.katakana}
+    first_name_kana              {person.first.katakana}
     birth_date              {Faker::Date.birthday(min_age: 5, max_age: 95)}
 
   end
