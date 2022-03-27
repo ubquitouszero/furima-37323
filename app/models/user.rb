18a5ffbddf9e9207ca21a86226aecad34aaf2e15
ubquitouszero <96573721+ubquitouszero@users.noreply.github.com>
2022-03-27 20:06:54 +0900

    二重のバリデーションを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name, presence: true
     validates :first_name, presence: true
     validates :last_name_kana, presence: true
