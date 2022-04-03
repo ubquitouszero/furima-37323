38cc7a2598cfe0771a20ad3791abd9ee98d089a2
ubquitouszero <96573721+ubquitouszero@users.noreply.github.com>
2022-04-03 14:49:26 +0900

    acrive_hashのバリデーション、アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :fee_status
+  belongs_to :prefecture
+  belongs_to :delivery_schedule
+
+  #空の投稿を保存できないようにする
+  # validates :name, :info, :price, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 , message: "please select category"}
+  validates :sales_status_id, numericality: { other_than: 1 , message: "please select sales satatus"}
+  validates :fee_status_id, numericality: { other_than: 1 , message: "please select fee method"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "please select prefecture"}
+  validates :delivery_schedule_id, numericality: { other_than: 1 , message: "please select delivery schedule"}
+
+
+
 end
--- db/migrate/20220403041518_create_items.rb
+++ db/migrate/20220403041518_create_items.rb
@@ -1,26 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name,           null: false
-      t.text :info,           null: false
-      t.integer :category_id,           null: false
-      t.string :name,           null: false
-      t.string :name,           null: false
-      t.string :name,           null: false
-      t.string :name,           null: false
-      t.string :name,           null: false
-      t.string :name,           null: false
-      t.string :name,           null: false
-
-      | name                 | string     | null: false                    |
-| info                 | text       | null: false                    |
-| category_id          | integer    | null: false                    |
-| sales_status_id      | integer    | null: false                    |
-| fee_status_id        | integer    | null: false                    |
-| prefecture_id        | integer    | null: false                    |
-| delivery_schedule_id | integer    | null: false                    |
-| price                | integer    | null: false                    |
-| user                 | references | null: false, foreign_key: true |
+      t.string      :name,                    null: false
+      t.text        :info,                    null: false
+      t.integer     :category_id,             null: false
+      t.integer     :sales_status_id,         null: false
+      t.integer     :fee_status_id,           null: false
+      t.integer     :prefecture_id,           null: false
+      t.integer     :delivery_schedule_id,    null: false
+      t.integer     :price,                   null: false
+      t.references  :user,                    null: false, foreign_key: true
       t.timestamps
     end
   end
