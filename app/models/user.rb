1a570c48a6da499b4061e735f8b74bc0691d73a2
ubquitouszero <96573721+ubquitouszero@users.noreply.github.com>
2022-04-03 10:47:59 +0900

    名前のカナにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name, presence: true
     validates :first_name, presence: true
+  end
+  with_options format: { with: /\A[ァ-ヶ一]+\z/, message: 'には全角カタカナを使用してください' } do
     validates :last_name_kana, presence: true
     validates :first_name_kana, presence: true
   end
