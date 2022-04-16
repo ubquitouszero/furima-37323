d6f0cdbe073e696cefe047594c718aae605de5ea
ubquitouszero <96573721+ubquitouszero@users.noreply.github.com>
2022-04-16 15:07:04 +0900

    検証用のコードを削除

--- app/javascript/calc_price.js
+++ app/javascript/calc_price.js
@@ -1,12 +1,9 @@
 window.addEventListener('load', () => {
   const price = document.getElementById("item-price")
   price.addEventListener('focusout', (e) => {
-    console.log('form1:focusout');
     price.innerHTML = price.value.toLocaleString();
   });
   price.addEventListener("keyup", () => {
-    // const priceSum  = document.getElementById("item-price");
-    // priceSum.innerHTML = price.value.toLocaleString();
     const commission = price.value * 0.1
     const profit = price.value - commission
     const commissionSum  = document.getElementById("add-tax-price");
--- app/models/item.rb
+++ app/models/item.rb
@@ -18,11 +18,11 @@ class Item < ApplicationRecord
   end
 
   #ジャンルの選択が「--」の時は保存できないようにする
-  # validates :category_id, numericality: { other_than: 1 , message: "は入力必須項目です。"}
-  # validates :sales_status_id, numericality: { other_than: 1 , message: "は入力必須項目です。"}
-  # validates :fee_status_id, numericality: { other_than: 1 , message: "は入力必須項目です。"}
-  # validates :prefecture_id, numericality: { other_than: 1 , message: "は入力必須項目です。"}
-  # validates :delivery_schedule_id, numericality: { other_than: 1 , message: "は入力必須項目です。"}
+  validates :category_id, numericality: { other_than: 0 , message: "は入力必須項目です。"}
+  validates :sales_status_id, numericality: { other_than: 0 , message: "は入力必須項目です。"}
+  validates :fee_status_id, numericality: { other_than: 0 , message: "は入力必須項目です。"}
+  validates :prefecture_id, numericality: { other_than: 0 , message: "は入力必須項目です。"}
+  validates :delivery_schedule_id, numericality: { other_than: 0 , message: "は入力必須項目です。"}
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -30,5 +30,5 @@ class Item < ApplicationRecord
   belongs_to :fee_status
   belongs_to :prefecture
   belongs_to :delivery_schedule
-  
+
 end
