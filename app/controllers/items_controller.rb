434663c95fda739f8fc937a9f3af70c49b5cd004
ubquitouszero <96573721+ubquitouszero@users.noreply.github.com>
2022-04-24 13:13:13 +0900

    Revert "商品削除機能"
    This reverts commit 93ac680c43e4407f665979903c6e5c56fce8a398.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :set_item, only: [:edit, :update, :show]
 
   def index
@@ -32,12 +32,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-  end
-
-  def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
   end
 
   private
