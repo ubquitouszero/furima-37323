1af18a0bce50c668532f791c91064a2c906d6e0f
ubquitouszero <96573721+ubquitouszero@users.noreply.github.com>
2022-04-24 15:05:54 +0900

    削除機能での冗長な記述を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,7 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if user_signed_in? && current_user.id == item.user_id
+    if current_user.id == item.user_id
       item.destroy
     end
     redirect_to root_path
